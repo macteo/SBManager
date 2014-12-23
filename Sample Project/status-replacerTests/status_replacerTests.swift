f3cc16bdf519585e3c070b168db183dfe3ab4256
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2014-12-23 18:41:07 +0100

    Added battery images, still not perfect, but we are near.

---
+++ Sample Project/status-replacerTests/status_replacerTests.swift
@@ -0,0 +1,36 @@
+//
+//  status_replacerTests.swift
+//  status-replacerTests
+//
+//  Created by Matteo Gavagnin on 04/12/14.
+//  Copyright (c) 2014 MacTeo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class status_replacerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
